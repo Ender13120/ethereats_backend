7aaedd63d84e4c942da7f16ac1049a7d401d6c2b
Ender <117198798+Ender13120@users.noreply.github.com>
2023-09-24 00:28:10 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { config } from 'dotenv';
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
