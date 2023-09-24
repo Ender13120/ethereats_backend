a72c9c88abdb58d39915f6df258b8aca21bef08a
Ender <117198798+Ender13120@users.noreply.github.com>
2023-09-23 23:06:42 -0400

    encrypt customer params

--- src/order-tracking/orderTrackerCronJob.service.ts
+++ src/order-tracking/orderTrackerCronJob.service.ts
@@ -3,6 +3,7 @@ import { Cron, CronExpression } from '@nestjs/schedule';
 import { PrismaService } from './PrismaService.service';
 import { ethers } from 'ethers';
 import EtherEatsABI from '../abi/DecentralizedDeliveryService.json';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable()
 export class OrderTrackerService {
@@ -11,6 +12,8 @@ export class OrderTrackerService {
     '0x03B99A03bd4b73E5c4FAb00C523A17C250BA7700';
   private readonly abi = EtherEatsABI.abi;
   private contract;
+
+  private key = process.env.AES_KEY;
 
   constructor(private prisma: PrismaService) {
     const provider = new ethers.providers.JsonRpcProvider(
@@ -21,6 +24,16 @@ export class OrderTrackerService {
       this.abi,
       provider,
     );
+  }
+
+  private encrypt(message) {
+    const iv = CryptoJS.lib.WordArray.random(128 / 8).toString(
+      CryptoJS.enc.Hex,
+    ); // Generate a random IV
+    const encrypted = CryptoJS.AES.encrypt(message, this.key, {
+      iv: CryptoJS.enc.Hex.parse(iv),
+    });
+    return iv + encrypted.ciphertext.toString(CryptoJS.enc.Hex); // Return the IV and the ciphertext
   }
 
   @Cron(CronExpression.EVERY_10_SECONDS)
@@ -65,8 +78,10 @@ export class OrderTrackerService {
               deliverer: order.deliverer,
               amount: order.amount.toString(),
               timestamp: order.timestamp.toString(),
-              encryptedParams:
-                '4358b158ab16ebc2371bb374885007dc76ed0b0a5d0b611bb7a427e61c9f8157', //ethers.utils.toUtf8String(order.encryptedParams), //Placeholder for AES Encryption App
+              encryptedParams: this.encrypt(
+                ethers.utils.toUtf8String(order.encryptedParams),
+              ),
+              // '4358b158ab16ebc2371bb374885007dc76ed0b0a5d0b611bb7a427e61c9f8157', //, //Placeholder for AES Encryption App
               isAccepted: order.isAccepted,
               isCompleted: order.isCompleted,
             },
