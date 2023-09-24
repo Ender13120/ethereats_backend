a2ea9f0b03c47492c2ddc2bb0d620a54b94346b2
Ender <117198798+Ender13120@users.noreply.github.com>
2023-09-24 04:08:19 +0200

    COrs issue frontend

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { config } from 'dotenv';
 async function bootstrap() {
   config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'localhost:3000',
+    methods: 'GET, HEAD, PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
